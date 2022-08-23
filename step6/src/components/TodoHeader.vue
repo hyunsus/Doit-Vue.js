e47e184fb77c749db1e4915acb629ff4d57dfa8f
hyunsus <dkrl0257@naver.com>
2022-08-23 09:43:34 +0900

    todolist 제목 추가

--- step6/src/components/TodoHeader.vue
+++ step6/src/components/TodoHeader.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+/* scoped = 스타일 정의를 해당 컴포넌트에만 적용
+
+  color = 애플리케이션 제목의 텍스트 색깔을 지정
+  font-weight = 애플리케이션 제목의 텍스트 굵기를 지정
+  margin = 애플리케이션 제목의 텍스트 여백을 지정
+*/
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>

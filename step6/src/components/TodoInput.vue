cef1e694d8efa28016e591cbdb8dc49b2b1befac
hyunsus <dkrl0257@naver.com>
2022-08-23 09:52:43 +0900

    todolist 할 일 입력 추가

--- step6/src/components/TodoInput.vue
+++ step6/src/components/TodoInput.vue
@@ -1,10 +1,72 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type what you have to do"
+      v-on:keyup.enter="addTodo"
+    />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      // input박스 안의 입력값 초기화 메서드
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+/*
+  outline = 할 일을 입력하는 인풋 박스의 선 스타일 지정
+  background = input 박스의 배경색 지정
+  height = input 박스의 높이 설정
+  line-height = input 박스에 입력되는 텍스트의 중앙 정렬을 위해 설정
+  border-radius = input 박스의 둥근 테두리 속성 설정
+  float = 할 일 추가 버튼이 표시될 위치 정의
+  vertical-align = 할 일 추가 아이콘의 수직 정렬 정의
+*/
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>

b5f7391fa0f4a6f5f8b467ee9605a06bf458b51e
hyunsus <dkrl0257@naver.com>
2022-08-23 10:17:01 +0900

    최상위 컴포넌트 css 수정

--- step6/src/App.vue
+++ step6/src/App.vue
@@ -22,6 +22,28 @@ export default {
   },
 };
 </script>
-
+<style>
+/*
+  text-align = 애플리케이션 전체에서 사용하는 텍스트의 정렬 방식을 선택
+  background-color = 애플리케이션 전체의 배경 색을 꾸미기 위해 지정
+  border-style = 할 일을 입력하는 인풋 박스의 테두리 모양을 정의
+  box-shadow = 할 일을 입력하는 인풋 박스와 할 일 아이템의 아래 그림자 정의
+  
+*/
+body {
+  text-align: center;
+  background-color: #f6f6f8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10 px rgba(0, 0, 0, 0.03);
+}
+</style>
 <style>
 </style>

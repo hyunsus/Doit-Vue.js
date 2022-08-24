c82714a66ca3395f669a52ecbf7e0665f4fb91d0
hyunsus <dkrl0257@naver.com>
2022-08-24 09:54:31 +0900

    뷰 모달 적용

--- step6/src/components/TodoInput.vue
+++ step6/src/components/TodoInput.vue
@@ -9,28 +9,43 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import modal from "./common/Modal.vue";
+
 export default {
   data() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
     addTodo() {
-      if (this.newTodoItem !== "") {
-        var value = this.newTodoItem && this.newTodoItem.trim();
+      if (this.newTodoItem !== "" && this.newTodoItem) {
+        var value = this.newTodoItem.trim();
         this.$emit("addTodo", value);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput() {
-      // input박스 안의 입력값 초기화 메서드
       this.newTodoItem = "";
     },
+  },
+  components: {
+    Modal: modal,
   },
 };
 </script>
